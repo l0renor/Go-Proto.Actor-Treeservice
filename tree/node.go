c9c95b993db0ad039619c1e143367a90d566b145
freinold <fabian.reinold@hm.edu>
2019-11-06 14:32:25 +0100

    First try at structuring the code with comments. Changed PIDs to pointers. Defined Insert, Error and Success Messages. Defined Receive and insert functions for actor Node.

--- tree/node.go
+++ tree/node.go
@@ -2,6 +2,8 @@ package tree
 
 import "github.com/AsynkronIT/protoactor-go/actor"
 
+// Actor Node --------------------------------------------
+
 type Node struct {
 	maxValues int
 	inner     Inner
@@ -9,11 +11,51 @@ type Node struct {
 }
 
 type Inner struct {
-	left    actor.PID
-	right   actor.PID
+	left    *actor.PID
+	right   *actor.PID
 	maxLeft int
 }
 
 type Leaf struct {
 	values map[int]string
+}
+
+// Messages -----------------------------------------------
+
+type Insert struct {
+	key    int
+	value  string
+	caller *actor.PID
+}
+
+type Error struct {
+	originalMsg interface{}
+}
+
+type Success struct {
+	key         int
+	value       string
+	originalMsg interface{}
+}
+
+// Actions ------------------------------------------------
+
+func (state *Node) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *Insert:
+		state.insert(msg, context)
+	}
+}
+
+func (state *Node) insert(msg *Insert, context actor.Context) {
+	if state.inner != (Inner{}) {
+		switch {
+		case msg.key > state.inner.maxLeft:
+			context.Send(state.inner.right, msg)
+		case msg.key < state.inner.maxLeft:
+			context.Send(state.inner.left, msg)
+		case msg.key == state.inner.maxLeft:
+			context.Send(msg.caller, &Error{originalMsg: msg})
+		}
+	}
 }
