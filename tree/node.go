1e67435e205ea3695882de8d448e92cdc4fae442
freinold <fabian.reinold@hm.edu>
2019-11-06 13:24:59 +0100

    Defined Node, Inner and Leaf struct.

--- tree/node.go
+++ tree/node.go
@@ -1,1 +1,19 @@
 package tree
+
+import "github.com/AsynkronIT/protoactor-go/actor"
+
+type Node struct {
+	maxValues int
+	inner     Inner
+	leaf      Leaf
+}
+
+type Inner struct {
+	left    actor.PID
+	right   actor.PID
+	maxLeft int
+}
+
+type Leaf struct {
+	values map[int]string
+}
