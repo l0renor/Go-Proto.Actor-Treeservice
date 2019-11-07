a5e6edd6e7bf61ebbccc2fc76d715a78b00ee11d
freinold <fabian.reinold@hm.edu>
2019-11-07 15:04:09 +0100

    Imported sort. Changed struct references in Node to Inner and Leaf to pointers. Added handling if node is leaf in insert message handler.

--- tree/node.go
+++ tree/node.go
@@ -1,13 +1,16 @@
 package tree
 
-import "github.com/AsynkronIT/protoactor-go/actor"
+import (
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"sort"
+)
 
 // Actor Node --------------------------------------------
 
 type Node struct {
-	maxValues int
-	inner     Inner
-	leaf      Leaf
+	maxElems int
+	inner    *Inner
+	leaf     *Leaf
 }
 
 type Inner struct {
@@ -48,7 +51,7 @@ func (state *Node) Receive(context actor.Context) {
 }
 
 func (state *Node) insert(msg *Insert, context actor.Context) {
-	if state.inner != (Inner{}) {
+	if state.inner != nil {
 		switch {
 		case msg.key > state.inner.maxLeft:
 			context.Send(state.inner.right, msg)
@@ -56,6 +59,41 @@ func (state *Node) insert(msg *Insert, context actor.Context) {
 			context.Send(state.inner.left, msg)
 		case msg.key == state.inner.maxLeft:
 			context.Send(msg.caller, &Error{originalMsg: msg})
+		}
+	} else if state.leaf != nil {
+		state.leaf.values[msg.key] = msg.value
+		if len(state.leaf.values) > state.maxElems {
+			// Leaf becomes inner node
+			state.inner = &Inner{}
+			state.inner.left = context.Spawn(actor.PropsFromProducer(func() actor.Actor {
+				return &Node{
+					maxElems: state.maxElems,
+					inner:    nil,
+					leaf:     &Leaf{values: make(map[int]string, state.maxElems)},
+				}
+			}))
+			state.inner.right = context.Spawn(actor.PropsFromProducer(func() actor.Actor {
+				return &Node{
+					maxElems: state.maxElems,
+					inner:    nil,
+					leaf:     &Leaf{values: make(map[int]string, state.maxElems)},
+				}
+			}))
+			keys := make([]int, state.maxElems)
+			for k := range state.leaf.values {
+				keys = append(keys, k)
+			}
+			sort.Ints(keys)
+			indexMaxLeft := state.maxElems / 2
+			state.inner.maxLeft = keys[indexMaxLeft]
+			for _, k := range keys {
+				if k <= indexMaxLeft {
+
+				} else {
+
+				}
+			}
+
 		}
 	}
 }
