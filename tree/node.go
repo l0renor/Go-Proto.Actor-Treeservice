838d3e4d85308d7cc9437a24d174ad52adb728b0
l0renor <leon-lukas11@web.de>
2019-11-10 13:25:54 +0100

    refactor messages

--- tree/node.go
+++ tree/node.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/ob-vss-ws19/blatt-3-chupa-chups/messages"
 	"math"
 	"sort"
 )
@@ -25,67 +26,40 @@ type Leaf struct {
 	values map[int]string
 }
 
-// Messages -----------------------------------------------
-type updateMaxleft struct {
-	newValue int
-}
-type Search struct {
-	caller *actor.PID
-	key    int
-}
-
-type Insert struct {
-	key   int
-	value string
-}
-type Delete struct {
-	key        int
-	caller     *actor.PID
-	needUpdate []*actor.PID
-}
-
-type Error struct {
-	originalMsg interface{}
-}
-
-type Success struct {
-	key         int
-	value       string
-	originalMsg interface{}
-}
-
 // Actions ------------------------------------------------
 
 func (state *Node) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
-	case *Insert:
+	case *messages.Insert:
 		state.insert(msg, context)
-	case *Search:
+	case *messages.Search:
 		state.search(msg, context)
-	case *Delete:
+	case *messages.Delete:
 		state.delete(msg, context)
-	case *updateMaxleft:
-		state.inner.maxLeft = msg.newValue
+	case *messages.UpdateMaxleft:
+		state.inner.maxLeft = msg.NewValue
+	case messages.Travers:
+		state.travers(&msg, context)
 
 	}
 }
 
-func (state *Node) insert(msg *Insert, context actor.Context) {
+func (state *Node) insert(msg *messages.Insert, context actor.Context) {
 	if state.inner != nil {
 		switch {
-		case msg.key > state.inner.maxLeft:
+		case msg.Key > state.inner.maxLeft:
 			context.RequestWithCustomSender(state.inner.right, msg, context.Sender())
-		case msg.key < state.inner.maxLeft:
+		case msg.Key < state.inner.maxLeft:
 			context.RequestWithCustomSender(state.inner.left, msg, context.Sender())
-		case msg.key == state.inner.maxLeft:
-			context.Send(context.Sender(), &Error{originalMsg: msg})
+		case msg.Key == state.inner.maxLeft:
+			context.Send(context.Sender(), &messages.Error{OriginalMsg: msg})
 		}
 	} else if state.leaf != nil {
-		_, ok := state.leaf.values[msg.key]
+		_, ok := state.leaf.values[msg.Key]
 		if ok {
-			context.Send(context.Sender(), &Error{originalMsg: msg})
+			context.Send(context.Sender(), &messages.Error{OriginalMsg: msg})
 		} else {
-			state.leaf.values[msg.key] = msg.value
+			state.leaf.values[msg.Key] = msg.Value
 			if len(state.leaf.values) > state.maxElems {
 				// Leaf becomes inner node
 				state.inner = &Inner{}
@@ -112,60 +86,78 @@ func (state *Node) insert(msg *Insert, context actor.Context) {
 				state.inner.maxLeft = keys[indexMaxLeft]
 				for _, k := range keys {
 					if k <= indexMaxLeft {
-						context.Request(state.inner.left, &Insert{key: keys[k], value: state.leaf.values[keys[k]]})
+						context.Request(state.inner.left, &messages.Insert{Key: keys[k], Value: state.leaf.values[keys[k]]})
 					} else {
-						context.Request(state.inner.right, &Insert{key: keys[k], value: state.leaf.values[keys[k]]})
+						context.Request(state.inner.right, &messages.Insert{Key: keys[k], Value: state.leaf.values[keys[k]]})
 					}
 				}
 			}
-			context.Send(context.Sender(), &Success{originalMsg: msg})
+			context.Send(context.Sender(), &messages.Success{OriginalMsg: msg})
 		}
 	}
 }
 
-func (state *Node) search(msg *Search, context actor.Context) {
+func (state *Node) search(msg *messages.Search, context actor.Context) {
 	if state.inner != nil { //IF is inner node
-		if msg.key > state.inner.maxLeft { // bigger -> keep searching on the right
+		if msg.Key > state.inner.maxLeft { // bigger -> keep searching on the right
 			context.Send(state.inner.right, msg)
 		} else {
 			context.Send(state.inner.left, msg) // smaller -> keep searching on the left
 		}
 	} else { // IF leaf
-		elem, ok := state.leaf.values[msg.key]
+		elem, ok := state.leaf.values[msg.Key]
 		if ok {
-			context.Send(msg.caller, Success{
-				key:         msg.key,
-				value:       elem,
-				originalMsg: msg,
+			context.Send(msg.Caller, messages.Success{
+				Key:         msg.Key,
+				Value:       elem,
+				OriginalMsg: msg,
 			})
 		} else { //Key not in Tree
-			context.Send(msg.caller, Error{originalMsg: msg})
+			context.Send(msg.Caller, messages.Error{OriginalMsg: msg})
 		}
 	}
 }
-func (node *Node) delete(msg *Delete, context actor.Context) {
+func (node *Node) delete(msg *messages.Delete, context actor.Context) {
 	if node.inner != nil { //IF is inner node
-		if msg.key <= node.inner.maxLeft { // search on left
-			if msg.key == node.inner.maxLeft { // update Maxleft
-				msg.needUpdate = append(msg.needUpdate, context.Self())
+		if msg.Key <= node.inner.maxLeft { // search on left
+			if msg.Key == node.inner.maxLeft { // update Maxleft
+				msg.NeedUpdate = append(msg.NeedUpdate, context.Self())
 			}
 			context.Send(node.inner.left, msg)
 		} else { // search on right
 			context.Send(node.inner.right, msg)
 		}
 	} else { //IF is leaf
-		_, OK := node.leaf.values[msg.key]
+		_, OK := node.leaf.values[msg.Key]
 		if OK {
-			delete(node.leaf.values, msg.key)
+			delete(node.leaf.values, msg.Key)
 			maxval := 0
 			for val := range node.leaf.values {
 				maxval = int(math.Max(float64(val), float64(maxval)))
 			}
-			for _, node := range msg.needUpdate {
-				context.Send(node, updateMaxleft{newValue: 1}) //TODO update with real value
+			for _, node := range msg.NeedUpdate {
+				context.Send(node, messages.UpdateMaxleft{NewValue: 1}) //TODO update with real value
 			}
 		} else {
-			context.Send(msg.caller, Error{originalMsg: msg})
+			context.Send(msg.Caller, messages.Error{OriginalMsg: msg})
 		}
+	}
+}
+
+func (node *Node) travers(msg *messages.Travers, context actor.Context) {
+	if node.inner != nil { //IF is inner node
+		context.Send(node.inner.right, messages.Travers{
+			Caller:     msg.Caller,
+			TreeValues: nil,
+		})
+		context.Send(node.inner.left, messages.Travers{
+			Caller:     msg.Caller,
+			TreeValues: nil,
+		})
+	} else { //IF is leaf
+		context.Send(msg.Caller, messages.Travers{
+			Caller:     msg.Caller,
+			TreeValues: node.leaf.values,
+		})
 	}
 }
