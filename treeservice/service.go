3d7a679a13274bebe00428766308307df9ab10c3
freinold <fabian.reinold@hm.edu>
2019-11-11 15:36:24 +0100

    Defined Service actor, started first protobuf messages.

--- treeservice/service.go
+++ treeservice/service.go
@@ -1,4 +1,24 @@
 package service
 
+import (
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/ob-vss-ws19/blatt-3-chupa-chups/tree"
+)
+
 type Service struct {
+	trees map[int32]Tree
+}
+
+type Tree struct {
+	Root  *actor.PID
+	Token string
+}
+
+func (service *Service) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case tree.Success:
+		handleSuccess(msg)
+	case tree.Error:
+		handleError(msg)
+	}
 }
